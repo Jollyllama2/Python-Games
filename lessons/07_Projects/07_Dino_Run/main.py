31e14e1958b5e680da86fbd7981fffb998db1987
Jollyllama2 <139662407+Jollyllama2@users.noreply.github.com>
2026-05-09 20:00:58 +0000

    qwert

--- lessons/07_Projects/07_Dino_Run/main.py
+++ lessons/07_Projects/07_Dino_Run/main.py
@@ -3,27 +3,36 @@ import random
 from pathlib import Path
 import time 
 
-SCREEN_WIDTH = 400
-SCREEN_HEIGHT = 600
+SCREEN_WIDTH = 590
+SCREEN_HEIGHT = 690
 SPEED = 45
 GRAVITY = 5
 GAME_SPEED = 25 
 
-GROUND_WIDTH = 2*SCREEN_HEIGHT
+GROUND_WIDTH = SCREEN_HEIGHT
 GROUND_HEIGHT = 120
 
 dd = Path(__file__).parent
+
 
 #class Dino()
     
 
 
+clock= pygame.time.Clock()
 
 
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption('Dino Run')
 
 
+BACKGROUND_1 =  pygame.image.load(dd / 'images/dino_party_sheet.png')
+BACKGROUND = pygame.transform.scale(BACKGROUND_1, (SCREEN_WIDTH, SCREEN_HEIGHT))
+while True: 
+    
+    
+    clock.tick(15)
 
+    screen.blit(BACKGROUND, (0,0))
 
-BACKGROUD =  pygame.image.load(dd / 'images/dino_party_sheet.png')
-BACKGROUND = pygame.transform.scale(BACKGROUD, (SCREEN_WIDTH, SCREEN_HEIGHT))
-while True:    
+    pygame.display.update()
